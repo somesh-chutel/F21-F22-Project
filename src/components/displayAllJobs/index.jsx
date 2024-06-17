134aefb99e7aa69cc10adcc8f9bdc366ce03fe59
srocknik <somesh.chutel90@gmail.com>
2024-06-17 19:48:12 +0530

    six commit

--- src/components/displayAllJobs/index.jsx
+++ src/components/displayAllJobs/index.jsx
@@ -1,17 +1,51 @@
+//react icons link : -  https://react-icons.github.io/react-icons/
+import { FaStar } from "react-icons/fa";
+import { FaLocationDot,FaBriefcase } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 import './index.css'
 
 
 const DisplayAllJobs = (props)=>{
+    const {jobsItem} = props
 
-    const {jobsItem} = props;
+    return (
 
+                <li className='jobs-card rounded'>
+                            
+                            <div className='web-logo-rating-cont'>
+                                <img className="jobs-web-logo" src={jobsItem.company_logo_url} alt="" />
 
-    return(
+                                <div className='rating-cont'>
+                                    <h3>{jobsItem.title}</h3>
+                                    <FaStar className="rating-icon"/>
+                                    <span>{jobsItem.rating}</span>
+                                </div>
+                            </div>
 
-        <li className='job-card'>
-                <h1> {jobsItem.title} </h1>
-        
-        </li>
+                            <div className="location-ppa-cont">
+
+                                    <div className="location-empt-cont">
+                                        <FaLocationDot className="me-1"/>
+                                        <span className="me-3">{jobsItem.location}</span>
+                                        <FaBriefcase className="me-1"/>
+                                        <span>{jobsItem.employment_type}</span>
+                                    </div>
+
+                                    <h5>{jobsItem.package_per_annum}</h5>
+
+                            </div>
+
+                            <hr />
+
+                            <div>
+
+                                    <h5>Description</h5>
+
+                                    <p>{jobsItem.job_description}</p>
+
+                            </div>
+
+                </li>
 
     )
 }
--- src/components/jobs/index.jsx
+++ src/components/jobs/index.jsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react';
-import Cookies from 'js-cookie';
-import Header from '../header';
-import DisplayAllJobs from '../displayAllJobs';
-import FilterSection from '../filterSection';
-import './index.css'
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import Header from "../header";
+import DisplayAllJobs from "../displayAllJobs";
+import FilterSection from "../filterSection";
+import "./index.css";
 
 // {
 //     "id": "bb95e51b-b1b2-4d97-bee4-1d5ec2b96751",
@@ -16,89 +16,82 @@ import './index.css'
 //     "package_per_annum": "10 LPA"
 //   }
 
+const Jobs = () => {
+  const token = Cookies.get("jwtToken");
+
+  const [allValues, setValues] = useState({
+    jobsList: [],
+    empType: [],
+    sllryRange: "",
+    searchInput: "",
+  });
+
+  useEffect(() => {
+    const fetchJobsData = async () => {
+      const api = `https://apis.ccbp.in/jobs?employment_type=${allValues.empType}&minimum_package=${allValues.sllryRange}&search=${allValues.searchInput}`;
+
+      const options = {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const response = await fetch(api, options);
+
+      const fetchData = await response.json();
+
+      if (response.ok === true) {
+        setValues({ ...allValues, jobsList: fetchData.jobs });
+      }
+    };
+
+    fetchJobsData();
+  }, [allValues.searchInput]);
+
+  const onChangeUserIn = (e) => {
+
+    if(e.key === "Enter"){
+
+        setValues({ ...allValues, searchInput: e.target.value });
+    }
+    
+  };
 
 
+  const onChangeempType = (value)=>{
 
-const Jobs = ()=>{
+    console.log(value);
 
-    const token = Cookies.get("jwtToken");
+  }
 
-    const [allValues,setValues] = useState({
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
 
-        jobsList : []
+      <div className="filter-alljobs-container">
+        <div className="filter-section-cont">
+          <FilterSection empTypeChangeFunct = {onChangeempType}/>
+        </div>
 
-    });
+        <div className="all-jobs-cont">
+          <input
+            onKeyDown={onChangeUserIn}
+            type="search"
+            className="form-control w-75 mb-3"
+          />
 
-
-    useEffect(()=>{
-
-        const fetchJobsData = async ()=>{
-
-            const api = "https://apis.ccbp.in/jobs";
-
-            const options = {
-                method: 'GET',
-                headers : {
-                    Authorization : `Bearer ${token}`
-                }
-              }
-
-            const response = await fetch( api, options );
-
-            const fetchData = await response.json();
-
-            if(response.ok === true){
-
-                setValues({...allValues,jobsList : fetchData.jobs})
-
-            }
-
-        }
-
-
-        fetchJobsData();
-
-
-    },[])
-
-
-
-    return(
-
-        <>
-            <div>
-                    
-                <Header/>
-
-
-            </div>
-
-            <div className='filter-alljobs-container'>
-
-                <div className='filter-section-cont'>
-                    <FilterSection/>
-                </div>
-
-                <div className='all-jobs-cont'>
-
-                    <ul>
-                        {
-                            allValues.jobsList.map( each => <DisplayAllJobs key={each.id} jobsItem = {each}/>)
-                        }
-
-                    </ul>
-
-                    
-
-                </div>
-
-            </div>
-        
-        </>
-
-    )
-}
-
-
+          <ul>
+            {allValues.jobsList.map((each) => (
+              <DisplayAllJobs key={each.id} jobsItem={each} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Jobs;
